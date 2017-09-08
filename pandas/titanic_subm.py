d31d6f01a41bc0b2244461f6cf5d049cdfc9567c
diegoami <diego.amicabile@gmail.com>
2017-09-08 14:51:09 +0200

    First algorithm

--- pandas/titanic_subm.py
+++ pandas/titanic_subm.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+import xgboost
+import os
+import sys
+import datetime
+from xgboost import XGBClassifier
+
+best_score, best_clf = 0, None
 
 def get_df(file):
     tdata = pd.read_csv(file)
@@ -37,32 +48,95 @@ def get_df(file):
 
     return tdata
 
-def get_clf_fit(train_df):
-    def get_X_y(data):
-        X = data.drop('Survived', axis=1)
-        y = data['Survived']
-        return (X, y)
 
-    X, y = get_X_y(train_df)
+def do_sol(clf, train_df, test_df, outputfile, preprocess = None, f = sys.stdout):
+    global best_score, best_clf
 
-    from sklearn import svm
-    clf = svm.SVC(kernel='linear', C=1)
-    clf.fit(X, y)
-    return clf
+    def get_clf_fit(clf, train_df):
+        global best_score, best_clf
 
-def print_sol(clf, test_df, output_file):
-    yp = clf.predict(test_df)
-    dsol = pd.DataFrame()
-    dsol['PassengerId'] = test_df['PassengerId']
-    dsol['Survived'] = yp
+        def get_X_y(data):
+            X = data.drop('Survived', axis=1)
+            y = data['Survived']
+            return (X, y)
 
-    dsol = dsol.set_index('PassengerId')
-    dsol.to_csv(output_file, index_label='PassengerId')
+        def print_score(clf, X, y):
+            global best_score, best_clf
+
+            scores = cross_val_score(clf, X, y, cv=5)
+            if (sum(scores) > best_score):
+                best_score = sum(scores)
+                best_clf = clf
+
+            print(scores, file=f)
+
+        X, y = get_X_y(train_df)
+        clf.fit(X, y)
+        print_score(clf,X,y)
+        return clf
+
+    def print_sol(clf, test_df, output_file, passenger_ids):
+        yp = clf.predict(test_df)
+        dsol = pd.DataFrame()
+        dsol['PassengerId'] = passenger_ids
+        dsol['Survived'] = yp
+
+        dsol = dsol.set_index('PassengerId')
+        dsol.to_csv(output_file, index_label='PassengerId')
+
+
+    passenger_ids = test_df['PassengerId']
+
+    if preprocess:
+        train_df = preprocess(train_df)
+        test_df  = preprocess(test_df)
+
+    if not os.path.isfile(outputfile):
+
+        print("Classifier initialized : {}".format(clf),file=f)
+        clf = get_clf_fit(clf, train_df)
+        print_sol(clf, test_df, outputfile, passenger_ids )
+        print("Printed result to file : {}".format(clf, outputfile),file=f)
+    else:
+
+        print("File {} already exists - Skipping".format(outputfile),file=f)
+
+def normalize_fields(data):
+    data = data.drop(['EmbarkedC', 'SibSp', 'Parch', 'Fare', 'PassengerId'], axis=1 )
+    return data
+
+def improve_fields(data):
+    data = normalize_fields(data)
+
+    def convAge(row):
+        if row['Age'] < 9:
+            return 0
+        elif row['Age'] < 14:
+            return 1
+        elif row['Age'] < 25:
+            return 2
+        else:
+            return 3
+
+    data['AgeC'] = data.apply(convAge, axis=1)
+    data = data.drop('Age', axis=1)
+    return data
 
 train_df =  get_df('../data/titanic/train.csv')
 test_df  =  get_df('../data/titanic/test.csv')
-clf      =  get_clf_fit(train_df )
-print_sol(clf, test_df, '../data/titanic/svm_submiss_1.csv')
 
+gen_fname = str(datetime.datetime.now()).replace(' ', '_') + '.log'
+f = open('../logs/' + gen_fname, 'w')
 
+#do_sol(svm.SVC(kernel='linear', C=1), train_df, test_df, '../data/titanic/svm_submiss_1.csv')
+#do_sol(RandomForestClassifier(n_estimators=50), train_df , test_df, '../data/titanic/random_forest_1.csv',f=f)
+#do_sol(RandomForestClassifier(n_estimators=50), train_df , test_df, '../data/titanic/random_forest_2.csv', normalize_fields,f=f)
+#for i in range(3,50):
+#    do_sol(RandomForestClassifier(n_estimators=i), train_df, test_df, '../data/titanic/random_forest_'+str(i)+'.csv', improve_fields,f=f)
+#    do_sol(KNeighborsClassifier(i), train_df , test_df, '../data/titanic/knn_'+str(i)+'.csv', improve_fields,f=f)
+do_sol(XGBClassifier(), train_df, test_df, '../data/titanic/xgb_0.csv', improve_fields, f=f)
 
+print('===== BEST CLASSIFIER =====')
+print(best_clf)
+print('===== BEST SCORE =====')
+print(best_score)
